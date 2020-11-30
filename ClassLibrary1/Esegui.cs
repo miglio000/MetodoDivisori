66e62604bca67a73c7ffc71ed17ca8b6560a36a7
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2020-11-30 10:45:19 +0100

    Add project files.

---
+++ ClassLibrary1/Esegui.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace LibreriaDivisori
+{
+    public class Esegui
+    {
+        public static void Divisori(int n)
+        {
+            int divisori;
+            for (int i = 1;i<=n; i++)
+            {
+                divisori = n / i;
+                if (n % i == 0)
+                {
+                    Console.WriteLine(divisori);
+                   
+                }
+            }
+            
+            
+        }
+    }
+}
---
+++ Divisori/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using LibreriaDivisori;
+namespace Divisori
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Inserisci un numero: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            Esegui.Divisori(n);
+            
+
+        }
+    }
+}
